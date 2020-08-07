f7dd645364db29fff5dbdecc4a7b14533db88e22
manuelp1996 <mepr1996@gmail.com>
2020-08-06 23:02:02 -0400

    Creación vista de listado de paquetes

--- pages/packs/_page/index.vue
+++ pages/packs/_page/index.vue
@@ -1,20 +1,68 @@
 <template>
-  <div></div>
+  <div>
+    <div class="container-fluid">
+      <div class="need-space"></div>
+      <div class="input-group">
+        <input
+          @keyup.enter="search"
+          v-model="query"
+          name="query"
+          type="text"
+          class="form-control-articulo"
+          placeholder="Buscar paquete"
+        />
+        <div class="input-group-append">
+          <span class="input-group-text">
+            <font-awesome-icon
+              :icon="['fas', 'search']"
+              @click="search"
+              class="icon-search-navbar"
+            />
+          </span>
+        </div>
+      </div>      
+      <div class="need-space"></div>
+      <div class="container-fluid">        
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"> <h3>Listado de Paquetes</h3> 
+            <div class="col-post">
+              <div class="head-col">                               
+              </div>
+              <div class="need-space"></div>
+              <div
+                v-for="(paquete,index) in paquetes"
+                :key="index"
+                class="row justify-content-center"
+              >
+                <CardPack
+                  v-bind:id="paquete.id"
+                  v-bind:titulo="paquete.titulo"
+                  v-bind:resumen="paquete.resumen"
+                  v-bind:precio="paquete.precio"
+                  v-bind:imagen="paquete.imagen"
+                />                
+              </div>
+              
+            </div>
+          </div>         
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
-
+import BannerCategorias from "~/components/BannerCategorias/BannerCategorias.vue";
+import CardPack from "~/components/Card/CardPack.vue";
+import CardArticulo from "~/components/Card/CardArticulo.vue";
 export default {
-  name: "RecentJavs",
   layout: (ctx) => (ctx.isMobile ? "mobile" : "default"),
-  components: {},
-  data() {
-    return {};
+  components: {
+    BannerCategorias,
+    CardPack,
+    CardArticulo,
   },
   head() {
     return {
-      title: "",
       meta: [
         {
           name: "description",
@@ -23,14 +71,23 @@ export default {
       ],
     };
   },
-  async asyncData({ params }) {
-    return {};
+  async asyncData({ store }) {
+    let paquetes;
+    await store.dispatch("getPaquetes").then((paq) => {
+      paquetes = paq;
+    });
+    let categorias;
+    await store.dispatch("getCategorias").then((cat) => {
+      categorias = cat;
+    });
+    return {
+      paquetes: paquetes.slice(0, 5),
+      categorias: categorias.slice(0, 6),
+    };
   },
   beforeCreate() {},
-  methods: {},
-  computed: {},
 };
 </script>
 
-<style lang="scss">
+<style>
 </style>
